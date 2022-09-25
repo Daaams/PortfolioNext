f0fbf8f53eb77f2bee84c1a564030d8401c6acf4
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2022-09-25 16:41:50 +0200

    little changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,16 @@
 import styles from '../styles/Navbar.module.css'
+import stylesLink from '../styles/linksStyle.module.css'
 import Link from 'next/link'
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
             <div>
-                <Link href={"/"}><a className={styles.link}>Home</a></Link>
-                <Link href={"/about"}><a className={styles.link}>About me</a></Link>
-                <Link href={"/technologies"}><a className={styles.link}>Technologies</a></Link>
-                <Link href={"/projects"}><a className={styles.link}>Projects</a></Link>
-                <Link href={"/contact"}><a className={styles.link}>Contact</a></Link>
+                <Link href={"/"}><a className={stylesLink.link}>Home</a></Link>
+                <Link href={"/about"}><a className={stylesLink.link}>About me</a></Link>
+                <Link href={"/technologies"}><a className={stylesLink.link}>Technologies</a></Link>
+                <Link href={"/projects"}><a className={stylesLink.link}>Projects</a></Link>
+                <Link href={"/contact"}><a className={stylesLink.link}>Contact</a></Link>
             </div>
         </nav>
     );
