cc341a21756d81d4ea0316071d8432d531f333ff
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2022-09-25 14:39:01 +0200

    add links and style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,15 @@
 import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
             <div>
-                <h1>Damien</h1>
-            </div>
-            <div>
-                <a className={styles.link}>Home</a>
-                <a className={styles.link}>About me</a>
-                <a className={styles.link}>Technologies</a>
-                <a className={styles.link}>Projects</a>
-                <a className={styles.link}>Contact</a>
+                <Link href={"/"}><a className={styles.link}>Home</a></Link>
+                <Link href={"/about"}><a className={styles.link}>About me</a></Link>
+                <Link href={"/technologies"}><a className={styles.link}>Technologies</a></Link>
+                <Link href={"/projects"}><a className={styles.link}>Projects</a></Link>
+                <Link href={"/contact"}><a className={styles.link}>Contact</a></Link>
             </div>
         </nav>
     );
