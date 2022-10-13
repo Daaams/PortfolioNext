bbfe2663cc2e39400976711af9edeab1b4bb62e0
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2022-10-13 22:57:07 +0200

    reorganised elements home page

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
         <title>Portfolio | Home</title>
         <meta name="description" content="Portfolio created by LEVRAULT Damien" />
         <link rel="icon" href="/favicon.ico" />
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
       </Head>
 
       <main className={styles.main}>
@@ -25,11 +27,14 @@ export default function Home() {
             <p>I am an IT student interested in web and mobile applications. I would like to make people's everyday life easier. </p>      
           </div>
           <figure  className={styles.myPicture}>
-            <Image src={"/moi.jpg"} width ={300} height={350} alt="my picture"/>
+            <Image src={"/moi2.png"} width ={600} height={550} alt="my picture"/>
           </figure>
         </div>
         <div className={styles.cv}>
           <p>Download my CV</p>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
           <p className={styles.downloadCV}>
             <button>HERE</button>
           </p>
